e3f45e62ab548f7df272a7c4b33bad2b490ec987
Alex Smith <alex@jalexsmith.com>
2019-07-02 19:37:21 -0500

    cleanerd

--- screenshot.js
+++ screenshot.js
@@ -1,5 +1,6 @@
 const { parse } = require('url');
 const { getIndeedSalary } = require('./chromium');
+const { getUrlFromPath } = require('./validator');
 
 module.exports = async function (req, res) {
     try {
@@ -9,7 +10,7 @@ module.exports = async function (req, res) {
             res.setHeader('Content-Type', 'text/html');
             res.status(400).send(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
         } else {
-            const stories = await getIndeedSalary(url);
+            const stories = await getIndeedSalary(url, type, qual, fullPage);
             res.setHeader('Content-Type', 'application/json');
             res.status(200).send(stories);
         }
