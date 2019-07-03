f9dcd3269cf483356ff3c55f4ddeeb49237deda9
Alex Smith <alex@jalexsmith.com>
2019-07-02 22:40:09 -0500

    better handling of more obscure jobs

--- chromium.js
+++ chromium.js
@@ -14,18 +14,23 @@ async function getIndeedSalary(url) {
 	await page.goto(url)
 	// execute standard javascript in the context of the page.
 	const stories = await page.evaluate(() => {
-	    const raw_results = Array.from(document.querySelectorAll('div.cmp-sal-salary'))
-	    var salaries = 0;
-	    var wages = 0;
-	    for (var i=0; i < raw_results.length; i++) {
-	        if(raw_results[i].textContent.indexOf('per year') !== -1) {
-	            salaries = salaries + parseFloat(raw_results[i].textContent.replace('per year', '').replace('$', '').replace(',', ''));
-	        } else if (raw_results[i].textContent.indexOf('per hour') !== -1) {
-	            wages = wages + parseFloat(raw_results[i].textContent.replace('per hour', '').replace('$', '').replace(',', ''));
-	        }				
-	    }
-	    salaries = salaries + ((wages*40)*50);
-	    return Math.round(salaries/raw_results.length);
+		if(document.querySelectorAll('div.cmp-not-enough-data-box').length >= 1) {
+			var salaries = document.querySelectorAll('div.cmp-not-enough-data-box strong')[0].textContent.replace('$', '').replace(',', '');
+		    return Math.round(salaries);
+		} else {
+			const raw_results = Array.from(document.querySelectorAll('div.cmp-sal-salary'));
+		    var salaries = 0;
+		    var wages = 0;
+		    for (var i=0; i < raw_results.length; i++) {
+		        if(raw_results[i].textContent.indexOf('per year') !== -1) {
+		            salaries = salaries + parseFloat(raw_results[i].textContent.replace('per year', '').replace('$', '').replace(',', ''));
+		        } else if (raw_results[i].textContent.indexOf('per hour') !== -1) {
+		            wages = wages + parseFloat(raw_results[i].textContent.replace('per hour', '').replace('$', '').replace(',', ''));
+		        }				
+		    }
+		    salaries = salaries + ((wages*40)*50);
+		    return Math.round(salaries/raw_results.length);
+		}	    
 	})
     
     await browser.close();
